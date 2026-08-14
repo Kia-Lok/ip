0ef6fdd74ca9dee43a3b26696092ac87b570598f
Kia-Lok <maxtan2605@gmail.com>
2026-08-15 03:04:28 +0800

    Add the ability to store and list entries

--- src/main/java/Walter.java
+++ src/main/java/Walter.java
@@ -20,18 +20,36 @@ public class Walter {
         System.out.println("What can I do for you?");
         System.out.println(separator);
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
             System.out.println(separator);
 
             if (command.equals("bye")) {
-                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println("Walter: Bye. Hope to see you again soon!");
                 System.out.println(separator);
                 break;
             }
 
-            System.out.println("Walter: " + command);
+            if (command.equals("list")) {
+                if (taskCount == 0) {
+                    System.out.println("There are currently no items on your list.");
+                } else {
+                    System.out.println("Here are the current items on your list:");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                }
+            } else if (taskCount < tasks.length) {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println("Walter has added " + command + " to the list.");
+            } else {
+                System.out.println("Walter: Your task list is full.");
+            }
+
             System.out.println(separator);
         }
     }
