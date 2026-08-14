be460d7be3112ebff20bf1b37e86d98f0274ea95
Kia-Lok <maxtan2605@gmail.com>
2026-08-15 02:43:13 +0800

    Change Walter's greeting and exit text

--- src/main/java/Walter.java
+++ src/main/java/Walter.java
@@ -3,6 +3,7 @@
  */
 public class Walter {
     public static void main(String[] args) {
+        String separator = "____________________________________________________________";
         String banner = """
                 ██╗    ██╗ █████╗ ██╗  ████████╗███████╗██████╗
                 ██║    ██║██╔══██╗██║  ╚══██╔══╝██╔════╝██╔══██╗
@@ -11,6 +12,12 @@ public class Walter {
                 ╚███╔███╔╝██║  ██║███████╗██║   ███████╗██║  ██║
                  ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚═╝   ╚══════╝╚═╝  ╚═╝
                 """;
+        System.out.println(separator);
         System.out.print(banner);
+        System.out.println("Howdy! I'm Walter!");
+        System.out.println("What are you looking for today?");
+        System.out.println(separator);
+        System.out.println("See you back here soon!");
+        System.out.println(separator);
     }
 }
